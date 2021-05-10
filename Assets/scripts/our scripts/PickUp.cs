dc1730f9871106c54f5f639b735d472bcd521878
Nordy4 <jordi.2000a@gmail.com>
2021-05-10 18:02:00 +0200

    Initial commit

---
+++ Assets/scripts/our scripts/PickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public bool grabbed;
+
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("Player2")){
+            grabbed = true;
+            this.transform.SetParent(other.transform, true);
+        }
+
+        if((other.gameObject.CompareTag("Cauldron"))&&(grabbed == true)){
+            Destroy(this.gameObject);
+        }
+    }
+}
